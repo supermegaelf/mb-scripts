c4f24f728c8fa035f085768cbc53038cfdf993bd
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-04-21 14:29:12 +0300

    Update sub.sh

--- distributed/sub.sh
+++ distributed/sub.sh
@@ -6,7 +6,7 @@ CONFIG_FILE="/opt/marzban/.env"
 
 mkdir -p "$TEMPLATE_DIR" &&
 wget -q https://raw.githubusercontent.com/supermegaelf/mb-pages/main/sub/index.html -O "$TEMPLATE_FILE" || {
-    echo "Error: failed to download the template"
+    echo "Error: failed to download the template";
     exit 1
 }
 
