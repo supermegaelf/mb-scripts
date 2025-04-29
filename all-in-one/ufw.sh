f965db4f14b941f82afda1a0acc1050d212986af
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-04-29 20:03:21 +0300

    Update ufw.sh

--- all-in-one/ufw.sh
+++ all-in-one/ufw.sh
@@ -11,10 +11,10 @@ mkdir -p /root/scripts && cd /root/scripts
 
 wget -q https://raw.githubusercontent.com/supermegaelf/mb-scripts/main/all-in-one/cf-ufw-pannel.sh -O /root/scripts/cf-ufw-pannel.sh
 
-chmod +x /root/scripts/cf-ufw.sh
+chmod +x /root/scripts/cf-ufw-pannel.sh
 
-bash /root/scripts/cf-ufw.sh
+bash /root/scripts/cf-ufw-pannel.sh
 
-echo "@daily root /root/scripts/cf-ufw.sh &> /dev/null" | sudo tee -a /etc/crontab
+echo "@daily root /root/scripts/cf-ufw-pannel.sh &> /dev/null" | sudo tee -a /etc/crontab
 
 ufw reload
