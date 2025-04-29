2ba84e239168967d2d78e7539f6abe6333a62bec
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-04-29 15:53:11 +0300

    Update and rename cf-ufw.sh to cf-ufw-pannel.sh

--- all-in-one/cf-ufw-pannel.sh
+++ all-in-one/cf-ufw-pannel.sh
@@ -5,9 +5,9 @@ curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips
 echo "" >> /tmp/cf_ips
 curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
 
-# Restrict traffic to ports 8443 (TCP) & 9200 (TCP)
+# Restrict traffic to ports 8443 (TCP)
 # UFW will skip a subnet if a rule already exists (which it probably does)
-for ip in $(cat /tmp/cf_ips); do ufw allow from "$ip" to any port 8443,9200 proto tcp comment 'Cloudflare'; done
+for ip in $(cat /tmp/cf_ips); do ufw allow from "$ip" to any port 8443 proto tcp comment 'Cloudflare'; done
 
 # Delete downloaded lists from above
 rm /tmp/cf_ips
